f2c11430ec27910923f7030a8bccb0232f886910
Akash raj(CMD) <akashraj9828@gmail.com>
2020-08-16 20:10:46 +0530

    Download added

--- src/Pages/Draw.js
+++ src/Pages/Draw.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect, useRef } from 'react';
 import './Draw.scss';
+import { download } from '../Util';
 
 const emptyMatrix = (size) =>
 	Array(size)
@@ -37,6 +38,8 @@ const Draw = (props) => {
 	}
 	`);
 
+	const [html, setHtml] = useState('');
+
 	useEffect(() => {
 		(async () => {
 			let shadow = [];
@@ -45,12 +48,12 @@ const Draw = (props) => {
 				row.forEach((cell, colIndex) => {
 					if (cell) {
 						shadow.push(`${colIndex * pixelSize}px ${rowIndex * pixelSize}px  ${cell}`);
-						svg_internal.push(`<rect width=${pixelSize} height=${pixelSize} style='transform:translate(${colIndex * pixelSize}px , ${rowIndex * pixelSize}px )' fill=${cell} />`);
+						svg_internal.push(`<rect width="${pixelSize}" height="${pixelSize}" style="transform:translate(${colIndex * pixelSize}px , ${rowIndex * pixelSize}px )" fill="${cell}" />`);
 					}
 				});
 			});
 
-			shadow = shadow.join(',') + ';';
+			shadow = shadow.join(',');
 			let new_css = `
 		#pixelart-html {
 			width: ${pixelSize * gridSize}px;
@@ -68,7 +71,7 @@ const Draw = (props) => {
 		}`;
 			setCss(new_css);
 
-			const final_svg = `<svg xmlns="http://www.w3.org/2000/svg" width=${pixelSize * gridSize} height=${pixelSize * gridSize} viewBox="0 0 ${pixelSize * gridSize} ${pixelSize * gridSize}">
+			const final_svg = `<svg xmlns="http://www.w3.org/2000/svg" width="${pixelSize * gridSize}" height="${pixelSize * gridSize}" viewBox="0 0 ${pixelSize * gridSize} ${pixelSize * gridSize}">
 								${svg_internal.join('\n')}
 							</svg>	`;
 
@@ -100,35 +103,89 @@ const Draw = (props) => {
 		})();
 	}, [css]);
 
+	const downloadHTML = async () => {
+		const text = ` 
+		<style>
+		${css}
+		</style>
+		<div id='pixelart-html'></div>
+		`;
+		download('pixelart.html', text);
+	};
+
 	const [dragging, setDragging] = useState(false);
 	return (
 		<Fragment>
 			<div className='settings'>
 				<input type='color' value={color} onChange={(e) => setColor(e.target.value)} />
-				<input type='range' min='4' max='20' value={gridSize} onChange={(e) => setGridSize(e.target.valueAsNumber)} />
+				<div className='grid-size'>
+					<button className='btn action-btn mx-1 decrement' onClick={() => setGridSize((s) => s - 1)}>
+						-
+					</button>
+					<span className='grid-size-value'>
+						{gridSize} x {gridSize}
+					</span>
+					<button className='btn action-btn mx-1 increment' onClick={() => setGridSize((s) => s + 1)}>
+						+
+					</button>
+				</div>
+				<div className='pixel-size'>
+					<button className='btn action-btn mx-1 decrement' onClick={() => setPixelSize((s) => s - 1)}>
+						-
+					</button>
+					<span className='pixel-size-value'>{pixelSize}px</span>
+					<button className='btn action-btn mx-1 increment' onClick={() => setPixelSize((s) => s + 1)}>
+						+
+					</button>
+				</div>
 			</div>
 
 			<div className='vs'>
-				<div id='pixelart-html' className='no-line-height'></div>
-				<div className='canvas noselect no-line-height' onSelect={() => false} onMouseDown={() => setDragging(true)} onMouseUp={() => setDragging(false)}>
-					{canvas.map((rowData, row) => {
-						return (
-							<div className='cellrow' key={row}>
-								{rowData.map((cell, col) => {
-									const localStyle = {
-										width: pixelSize,
-										height: pixelSize,
-										background: cell,
-									};
-									return <div key={`${row}+${col}`} className='cell noselect' style={localStyle} onClick={(e) => fillCell(row, col, e)} onMouseOver={(e) => dragging && fillCell(row, col, e)} />;
-								})}
-							</div>
-						);
-					})}
+				<div className='art-container d-none'>
+					<span className='art-header'>
+						HTML{' '}
+						<button className='btn action-btn mx-1 html-download' onClick={downloadHTML}>
+							{' '}
+							Download{' '}
+						</button>
+					</span>
+					<div id='pixelart-html' className='no-line-height'></div>
 				</div>
-				<div id='pixelart-svg' className='no-line-height' ref={svgElement}></div>
+				<div className='art-container'>
+					<span className='art-header'>YOUR CANVAS</span>
+					<div className='canvas noselect no-line-height' onSelect={() => false} onMouseDown={() => setDragging(true)} onMouseUp={() => setDragging(false)}>
+						{canvas.map((rowData, row) => {
+							return (
+								<div className='cellrow' key={row}>
+									{rowData.map((cell, col) => {
+										const localStyle = {
+											width: pixelSize,
+											height: pixelSize,
+											background: cell,
+										};
+										return <div key={`${row}+${col}`} className='cell noselect' style={localStyle} onClick={(e) => fillCell(row, col, e)} onMouseOver={(e) => dragging && fillCell(row, col, e)} />;
+									})}
+								</div>
+							);
+						})}
+					</div>
+				</div>
+				<div className='art-container d-none'>
+					<span className='art-header'>
+						SVG{' '}
+						<button
+							className='btn action-btn mx-1 svg-download'
+							onClick={() => {
+								download('pixelart.svg', svg);
+							}}>
+							{' '}
+							Download{' '}
+						</button>{' '}
+					</span>
+					<div id='pixelart-svg' className='no-line-height' ref={svgElement}></div>
+				</div>
 			</div>
-			<div className='output'>
+			<div className='output d-none'>
 				<pre className='html'>{"<div id='pixelart-html'></div>"}</pre>
 				<pre className='css'>{css}</pre>
 			</div>
@@ -138,5 +195,4 @@ const Draw = (props) => {
 
 export default Draw;
 
-function aaa(sda, asdad) {}
 // Inconsolata, Monaco, Consolas, 'Courier New', Courier
